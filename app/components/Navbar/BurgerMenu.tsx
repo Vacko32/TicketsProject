aa4de617601dc8d04011a85261a1fddad9bfdd67
Martin Vaculik <mvaculik4@gmail.com>
2024-06-12 17:24:34 +0200

    burgermenu open close

--- app/components/Navbar/BurgerMenu.tsx
+++ app/components/Navbar/BurgerMenu.tsx
@@ -1,40 +1,25 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import Collums from "./Collums";
 
-const BurgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+interface BurgerMenuProps {
+  toogleMenu: () => void;
+}
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
+const BurgerMenu: React.FC<BurgerMenuProps> = ({ toogleMenu }) => {
+  // Correctly destructure the prop
   return (
     <div>
       <div>
         <div
           className="text-2xl font-semibold cursor-pointer sm:hidden"
-          onClick={toggleMenu}
+          onClick={toogleMenu}
         >
           <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
-      {isOpen && (
-        <div className="fixed inset-0 z-50 bg-sushigray flex flex-col border border-slate-700 items-center justify-center">
-          <div className="flex flex-row gap-6">
-            <div>sdadsa</div>
-            <div className="cursor-pointer" onClick={toggleMenu}>
-              dfd
-            </div>
-          </div>
-          <div>sd</div>
-          <div>sd</div>
-          <div>sd</div>
-        </div>
-      )}
     </div>
   );
 };
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useState } from "react";
 import BurgerMenu from "./BurgerMenu";
 import Collums from "./Collums";
 import Container from "./Container";
 import Logo from "./Logo";
-
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toogleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log("Menu toogled");
+  };
+
   return (
     <div>
       <div className="sm:block py-3 border-slate-700 border-b-[1px] bg-sushidarkblue">
@@ -14,11 +24,18 @@ const Navbar = () => {
             </div>
             <Collums />
             <div className="sm:hidden ml-auto">
-              <BurgerMenu />
+              <BurgerMenu toogleMenu={toogleMenu} />
             </div>
           </div>
         </Container>
       </div>
+      {isOpen && (
+        <div className="fixed inset-0 bg-sushidarkblue z-50 ">
+          <div className="flex items-start justify-end p-4 cursor-pointer text-white text-3xl border-b-2">
+            <FontAwesomeIcon icon={faXmark} onClick={toogleMenu} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
